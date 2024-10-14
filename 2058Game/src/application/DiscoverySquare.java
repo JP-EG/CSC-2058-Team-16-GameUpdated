56eb8e4bdc77fb1f794b341a13fbdb606587b7ef
Jakub <jakub78b@gmail.com>
2024-10-14 19:31:25 +0100

    Fixed formatting in the Discovery, Setback and Task square

--- 2058Game/src/application/DiscoverySquare.java
+++ 2058Game/src/application/DiscoverySquare.java
@@ -5,48 +5,44 @@ import java.util.List;
 import java.util.Random;
 
 public class DiscoverySquare {
-	
+
 	private List<Discovery> discoveries;
-	
+
 	public DiscoverySquare() {
 
 		discoveries = new ArrayList<>();
-		
+
 		discoveries.add(
-			new Discovery(
-				1,
-				"Generous Donation",
-				"A generous donor has come forward to support your cause."
-				+ "\nYou receive a donation of $2,000 to fund your project.",
-				"Budget",
-				2000
-		));
+				new Discovery(
+						1,
+						"Generous Donation",
+						"A generous donor has come forward to support your cause."
+								+ "\nYou receive a donation of $2,000 to fund your project.",
+						"Budget",
+						2000));
 		discoveries.add(
-			new Discovery(
-				2,
-				"Abundant Materials Source",
-				"You've uncovered a hidden stash of construction materials."
-				+ "\nGain 3 additional materials units for your project.",
-				"Material Units",
-				3
-		));
+				new Discovery(
+						2,
+						"Abundant Materials Source",
+						"You've uncovered a hidden stash of construction materials."
+								+ "\nGain 3 additional materials units for your project.",
+						"Material Units",
+						3));
 		discoveries.add(
-			new Discovery(3,
-			"Community Enthusiasm",
-				"The local community is excited about your project."
-				+ "\nGain 3 extra community trust points to show their support.",
-				"Community Trust Points",
-				3
-		));
+				new Discovery(3,
+						"Community Enthusiasm",
+						"The local community is excited about your project."
+								+ "\nGain 3 extra community trust points to show their support.",
+						"Community Trust Points",
+						3));
 		discoveries.add(
-			new Discovery(4,
-			"Technical Breakthrough",
-				"A technical breakthrough helps you in equipment procurement."
-				+ "\nYou save $1,500 from your budget.",
-				"Budget",
-				1500
-		));
-		
+				new Discovery(4,
+						"Technical Breakthrough",
+						"A technical breakthrough helps you in equipment procurement."
+								+ "\nYou save $1,500 from your budget.",
+						"Budget",
+						1500));
+
 	}
 
 	public Discovery randomDiscovery() {
--- 2058Game/src/application/SetbackSquare.java
+++ 2058Game/src/application/SetbackSquare.java
@@ -5,37 +5,65 @@ import java.util.List;
 import java.util.Random;
 
 public class SetbackSquare {
-	
+
 	private List<Setback> setbacks;
 
 	public SetbackSquare() {
-		
+
 		setbacks = new ArrayList<>();
 
-		Setback s1 = new Setback(1, "Eqipment break down", "Deduct 4 Equipment Efficiency Points from the player's inventory.",
-				"Consequence: Display a message that construction tasks are delayed. For example,\n"
-				+ "Equipment breakdown has caused a delay in construction. Please allocate additional time for task completion.", "Equipment Efficiency Points", 4);
-
-		Setback s2 = new Setback(2, "Budget Cutback", "Deduct $3,000 from the player's budget", "Consequence: Inform the player that their budget is reduced, \n"
-				+ "limiting spending on equipment procurement tasks. Adjust your spending accordingly.", "Budget", 3000);
-
-		Setback s3 = new Setback(3, "Weather Delay", "Deduct 2 Time Units from the player's inventory.","Consequence: Communicate that weather-related delays will extend the time required for task completion.", "Time Units", 2);
-
-		Setback s4 = new Setback(4, "Technical Difficulties", "Deduct 2 Technical Expertise (Resource) from the player's inventory.", "Consequence: Inform the player that additional technical expertise is required \n"
-				+ " to ensure the proper connection of the solar system.", "Technical Expertise", 2);
-
-		Setback s5 = new Setback(5, "Community Dispute", "Deduct 2 Community Engagement (Resource) from the player's inventory.", "Consequence: Let the player know that unresolved community disputes can\n"
-				+ " lead to a decrease in community trust points.", "Community Engagement",  2);
-
-		Setback s6 = new Setback(6, "Material Shortage", "Deduct 2 Materials Units from the player's inventory.", "Consequence: Communicate that resource shortages affect construction progress and may delay task completion."
-				, "Material Units", 2);
-
-		setbacks.add(s1);
-		setbacks.add(s2);
-		setbacks.add(s3);
-		setbacks.add(s4);
-		setbacks.add(s5);
-		setbacks.add(s6);
+		setbacks.add(
+				new Setback(
+						1,
+						"Eqipment break down",
+						"Deduct 4 Equipment Efficiency Points from the player's inventory.",
+						"Consequence: Display a message that construction tasks are delayed. For example,\n"
+								+ "Equipment breakdown has caused a delay in construction. Please allocate additional time for task completion.",
+						"Equipment Efficiency Points",
+						4));
+		setbacks.add(
+				new Setback(
+						2,
+						"Budget Cutback",
+						"Deduct $3,000 from the player's budget",
+						"Consequence: Inform the player that their budget is reduced, \n"
+								+ "limiting spending on equipment procurement tasks. Adjust your spending accordingly.",
+						"Budget",
+						300));
+		setbacks.add(
+				new Setback(
+						3,
+						"Weather Delay",
+						"Deduct 2 Time Units from the player's inventory.",
+						"Consequence: Communicate that weather-related delays will extend the time required for task completion.",
+						"Time Units",
+						2));
+		setbacks.add(
+				new Setback(
+						4,
+						"Technical Difficulties",
+						"Deduct 2 Technical Expertise (Resource) from the player's inventory.",
+						"Consequence: Inform the player that additional technical expertise is required \n"
+								+ " to ensure the proper connection of the solar system.",
+						"Technical Expertise",
+						2));
+		setbacks.add(
+				new Setback(
+						5,
+						"Community Dispute",
+						"Deduct 2 Community Engagement (Resource) from the player's inventory.",
+						"Consequence: Let the player know that unresolved community disputes can\n"
+								+ " lead to a decrease in community trust points.",
+						"Community Engagement",
+						2));
+		setbacks.add(
+				new Setback(
+						6,
+						"Material Shortage",
+						"Deduct 2 Materials Units from the player's inventory.",
+						"Consequence: Communicate that resource shortages affect construction progress and may delay task completion.",
+						"Material Units",
+						2));
 
 	}
 
@@ -45,7 +73,5 @@ public class SetbackSquare {
 
 		return setbacks.get(randomIndex);
 	}
-
-	
 
 }
